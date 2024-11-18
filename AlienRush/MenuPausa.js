c1e6ea6d92e79622b76725b4b82a02dd570eb884
dperezn2021 <d.perezn.2021@alumnos.urjc.es>
2024-11-18 13:40:57 +0100

    PAUSA

--- AlienRush/MenuPausa.js
+++ AlienRush/MenuPausa.js
@@ -11,18 +11,25 @@ var MenuPausa = new Phaser.Class({
     },
 
     preload: function () {
-        this.load.image('fondoPause', 'assets/MenuPausa/fondoPausa.png');
+        this.load.image('fondoPausa', 'assets/MenuPausa/fondoPausa.png');
         this.load.image('botonReanudar', 'assets/MenuPausa/BotonReanudar.png');
         this.load.image('botonMenu', 'assets/MenuPausa/BotonMenu.png');
-        this.load.image('botonOpciones', 'assets/MenuPausa/BotonOpciones.png');
     },
 
     create: function () {
-        // Fondo del menú de pausa
-        this.add.image(400, 300, 'fondoPause');
-
-        // Botón de Reanudar
-        let botonReanudar = this.add.image(400, 300, 'botonReanudar').setInteractive();
+         /************************* FONDO *************************/
+        //IMAGEN
+        this.add.image(875, 440, 'fondoPausa');
+        //AUDIO
+        
+        
+        /************************* VARIABLES *************************/
+        let botonReanudar = this.add.image(875, 440, 'botonReanudar')       
+        let botonMenu = this.add.image(875, 590, 'botonMenu')
+        
+        /************************* BOTONES *************************/
+        //REANUDAR
+        botonReanudar.setInteractive();
         botonReanudar.on('pointerdown', () => {
             console.log("Reanudando MainGame...");
             this.scene.resume('MainGame');
@@ -30,21 +37,12 @@ var MenuPausa = new Phaser.Class({
             this.scene.stop('MenuPausa');
         });
 
-        // Botón de Menú Principal
-        let botonMenu = this.add.image(400, 400, 'botonMenu').setInteractive();
+        //MENU       
+        botonMenu.setInteractive();
         botonMenu.on('pointerdown', () => {
             console.log("Regresando al menú principal...");
-            this.musica.stop();
             this.scene.stop('MainGame');
             this.scene.start('MenuScene');  // Volver al menú principal
-        });
-
-        // Botón de Opciones
-        let botonOpciones = this.add.image(400, 500, 'botonOpciones').setInteractive();
-        botonOpciones.on('pointerdown', () => {
-            console.log("Abriendo Opciones desde PauseMenu...");
-            this.scene.launch('OpcionesPausa', { musica: this.musica });
-            this.scene.pause();  // Pausa la escena actual (MenuPausa) y abre OpcionesPausa
         });
 
     }
